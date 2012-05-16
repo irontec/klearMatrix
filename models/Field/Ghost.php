98ed2d774b309c59468abbf4bf77ea99cc25d79f
David Lores <david@irontec.com>
2012-05-16 09:42:30 +0000

    Opcion de condiciones en campo Ghost
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@89 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Ghost.php
+++ models/Field/Ghost.php
@@ -8,6 +8,8 @@
       default: true -> Si ponemos este parámetro, pasamos al ghost el valor del campo en el que estamos
       field: modelId -> Si ponemos field, pasamos al ghost el valor del campo que pongamos en field
       cache: true -> Poniendo cache, si el valor que se vaya a pasar al ghost ya se había pasado, se devuelve el resultado cacheado
+      conditions:
+        campo: valor -> Se hace el ghost si cumple las condiciones. Si no cumple las condiciones, se devuelve el valor del campo
 
     Si no ponemos ni default ni field, pasamos al ghost el primaryKey del registro en el que estemos.
 
@@ -19,9 +21,26 @@ class KlearMatrix_Model_Field_Ghost extends KlearMatrix_Model_Field_Abstract
 {
 
     protected $_cache = array();
+    protected $_condition = true;
 
     public function getCustomGetterName($model)
     {
+        $this->_condition = true;
+
+        if ($this->_config->getProperty('source')->conditions) {
+
+            foreach ($this->_config->getProperty('source')->conditions as $key => $condition) {
+
+                $get = 'get' . $model->columnNameToVar($key);
+                $res = $model->$get();
+                if ($res != $condition) {
+
+                    $this->_condition = false;
+                    break;
+                }
+            }
+        }
+
         if ($this->_config->getProperty('source')->default) {
             //Si existe el parámetro default, devolvemos el getter default
             return $this->_column->getGetterName($model, true);
@@ -37,6 +56,11 @@ class KlearMatrix_Model_Field_Ghost extends KlearMatrix_Model_Field_Abstract
 
     public function prepareValue($rValue, $result)
     {
+        if ($this->_condition === false) {
+
+            return $rValue;
+        }
+
         //Cogemos class y method para enviar el resultado
         $class = $this->_config->getProperty('source')->class;
         $method = $this->_config->getProperty('source')->method;

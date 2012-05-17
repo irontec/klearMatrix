3d7e6da8ddf666a887868a3fe3ffe739b268ad57
David Lores <david@irontec.com>
2012-05-17 09:41:47 +0000

    Modificado campo ghost para pasar todo el model al metodo del ghost
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@93 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Ghost.php
+++ models/Field/Ghost.php
@@ -3,17 +3,13 @@
 /**
  * Clase de campo tipo Ghost. Configuración en yaml para el campo.
     source:
-      class: Application_Model_GhostTerminals -> Clase para hacer nuestras cosas con el valor del campo
-      method: getManufacturer -> Método de la clase a la que vamos a enviar el valor del campo
-      default: true -> Si ponemos este parámetro, pasamos al ghost el valor del campo en el que estamos
-      field: modelId -> Si ponemos field, pasamos al ghost el valor del campo que pongamos en field
-      cache: true -> Poniendo cache, si el valor que se vaya a pasar al ghost ya se había pasado, se devuelve el resultado cacheado
-      cacheConditions:
+      class: Application_Model_GhostTerminals -> Clase para hacer nuestras cosas con el model del registro
+      method: getManufacturer -> Método de la clase a la que vamos a enviar el model del registro
+      field: modelId -> Si ponemos field, pasamos al ghost el valor del campo que pongamos en field. Si no se pone, se pasa el primaryKey
+      cache:
         campo: true -> Los valores de estos campos se añaden al valor del campo para comprobar la cache
       conditions:
         campo: valor -> Se hace el ghost si cumple las condiciones. Si no cumple las condiciones, se devuelve el valor del campo
-
-    Si no ponemos ni default ni field, pasamos al ghost el primaryKey del registro en el que estemos.
 
 * @author David Lores
 *
@@ -23,13 +19,22 @@ class KlearMatrix_Model_Field_Ghost extends KlearMatrix_Model_Field_Abstract
 {
 
     protected $_cache = array();
-    protected $_cacheConditions = '';
-    protected $_condition = true;
 
     public function getCustomGetterName($model)
     {
-        $this->_condition = true;
-        $this->_conditionCache = '';
+
+        if ($this->_config->getProperty('source')->field) {
+
+            //Si existe el parámetro field, devolvemos el getter para ese campo
+            $method = 'get' . $model->columnNameToVar($this->_config->getProperty('source')->field);
+            return $method;
+        }
+
+        return 'getPrimaryKey';
+    }
+
+    public function prepareValue($rValue, $model)
+    {
 
         //Comprobamos si se cumplean las condiciones para hacer el ghost
         if ($this->_config->getProperty('source')->conditions) {
@@ -40,60 +45,42 @@ class KlearMatrix_Model_Field_Ghost extends KlearMatrix_Model_Field_Abstract
                 $res = $model->$get();
                 if ($res != $condition) {
 
-                    $this->_condition = false;
-                    break;
+                    return $rValue;
                 }
             }
         }
 
         //Añadimos a las condiciones de cache si existen
-        if ($this->_config->getProperty('source')->cacheConditions) {
+        $cache = '';
+        if ($this->_config->getProperty('source')->cache) {
 
-            foreach ($this->_config->getProperty('source')->cacheConditions as $key => $condition) {
+            foreach ($this->_config->getProperty('source')->cache as $key => $condition) {
 
                 $get = 'get' . $model->columnNameToVar($key);
                 $res = $model->$get();
-                $this->_cacheConditions .= '-' . $res;
+                $cache .= $key . $res;
             }
-        }
-
-        if ($this->_config->getProperty('source')->default) {
-            //Si existe el parámetro default, devolvemos el getter default
-            return $this->_column->getGetterName($model, true);
-
-        } elseif ($this->_config->getProperty('source')->field) {
-            //Si existe el parámetro field, devolvemos el getter para ese campo
-            return 'get' . $model->columnNameToVar($this->_config->getProperty('source')->field);
-        }
-
-        //Si no existe field, devolvemos para coger el primaryKey
-        return 'getPrimaryKey';
-    }
-
-    public function prepareValue($rValue, $result)
-    {
-        if ($this->_condition === false) {
-
-            return $rValue;
         }
 
         //Cogemos class y method para enviar el resultado
         $class = $this->_config->getProperty('source')->class;
         $method = $this->_config->getProperty('source')->method;
 
-        $keyCache = md5($class . '-' . $method . $this->_cacheConditions);
+        $md5cache = md5($cache);
+        $md5method = md5($class . $method);
 
-        if ($this->_config->getProperty('source')->cache) {
-            if (isset($this->_cache[$keyCache][$rValue])) {
-                return $this->_cache[$keyCache][$rValue];
-            }
+        if ($this->_config->getProperty('source')->cache
+            && isset($this->_cache[$md5method][$md5cache])) {
+
+                return $this->_cache[$md5method][$md5cache];
         }
 
         $ghost = new $class;
-        $value = $ghost->{$method}($rValue);
+        $value = $ghost->{$method}($model);
 
         if ($this->_config->getProperty('source')->cache) {
-            $this->_cache[$keyCache][$rValue] = $value;
+
+            $this->_cache[$md5method][$md5cache] = $value;
         }
 
         return $value;

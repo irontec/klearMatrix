2a71e481cd075a35f507d32841391e0fe49d7fbb
David Lores <david@irontec.com>
2012-06-06 18:23:58 +0000

    Bug en Dialog
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@140 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Dialog.php
+++ models/Dialog.php
@@ -6,18 +6,56 @@
 *
 */
 class KlearMatrix_Model_Dialog extends KlearMatrix_Model_ResponseItem {
-	
+
 	protected $_type = 'dialog';
-	
+
 	public function setDialogName($name) {
 		$this->setItemName($name);
 	}
-	
-	public function setConfig(Zend_Config $config)
-	{
-	    $this->_config = new Klear_Model_KConfigParser;
-	    $this->_config->setConfig($config);
-	
-	    $this->_title = $this->_config->getProperty("title",false);
-	}
+
+    public function setConfig(Zend_Config $config)
+    {
+
+        $this->_config = new Klear_Model_KConfigParser;
+        $this->_config->setConfig($config);
+
+        $this->_mapper = $this->_config->getProperty("mapper",false);
+        $this->_modelFile = $this->_config->getProperty("modelFile",false);
+
+        $this->_filteredField = $this->_config->getProperty("filterField",false);
+
+        $this->_filterClass = $this->_config->getProperty("filterClass",false);
+
+        $this->_forcedValues = $this->_config->getProperty("forcedValues",false);
+
+        $this->_forcedPk = $this->_config->getProperty("forcedPk",false);
+
+        $this->_calculatedPkConfig = $this->_config->getProperty("calculatedPk",false);
+
+        $this->_plugin = $this->_config->getProperty("plugin", false);
+
+        $this->_title = $this->_config->getProperty("title",false);
+
+        $this->_hooks = $this->_config->getProperty("hooks", array());
+
+        $this->_customTemplate = $this->_config->getProperty("template", false);
+
+        $this->_customScripts = $this->_config->getProperty("scripts", false);
+
+        $this->_actionMessages = $this->_config->getProperty("actionMessages", false);
+
+        $this->_hasInfo = (bool)$this->_config->getProperty("info", false);
+        if ($this->_hasInfo) {
+            $this->_fieldInfo = new KlearMatrix_Model_Info;
+            $this->_fieldInfo->setConfig($this->_config->getProperty("info",false));
+        }
+
+        if ($this->_modelFile) {
+            $this->_parseModelFile();
+        }
+
+        if ($this->_mapper) {
+            $this->_checkClasses(array("_mapper"));
+        }
+    }
 }

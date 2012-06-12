7b7b7b9b3f7708e65e7e140365da6a33f3f9dcfd
Lander Ontoria <lander@irontec.com>
2012-06-12 16:41:29 +0000

    Quitando morralla editor html principal TINY, no hay más! he borrado los viejos.
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@150 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

---
+++ assets/js/plugins/jquery.ui.kleartinymce.js
@@ -0,0 +1,47 @@
+(function($, undefined) {
+	
+	$.widget('ui.kleartinymce', {
+		options: {
+		},
+		_init:function(){
+			
+			var $el = $(this.element);
+			
+			var options = this.options;
+			
+			var _self = this;
+			
+			var _contentChange = function(instance) {
+				var changed = !(instance.startContent == instance.getBody().innerHTML); 
+				if (changed) {
+					$("#"+instance.editorId).trigger('manualchange');
+				}
+			};
+			
+			var tinySettings = {
+				onchange_callback : function(instance) {
+					_contentChange(instance);
+				},
+				handle_event_callback :function (event, instance) {
+					switch (event.type) {
+						case 'keyup':
+						case 'click':
+							_contentChange(instance);
+							break;
+					}
+					return true;
+				},
+				init_instance_callback : function (instance) {
+					
+				}
+			};
+			
+			$.extend(tinySettings, options);
+			
+			
+			$el.tinymce(tinySettings);
+			
+		}
+			
+	});
+})(jQuery);	

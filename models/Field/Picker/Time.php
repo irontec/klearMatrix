82d346122cc5a4ae4284e524075ee210190ee5de
Javier Infante <jabi+github@irontec.com>
2012-06-14 11:03:10 +0000

    php 5.4 type hinting error solved
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@153 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Picker/Time.php
+++ models/Field/Picker/Time.php
@@ -1,6 +1,5 @@
 <?php
-//TODO: lang global getter (lander donde andaba eso¿)
-$lang = 'es';
+
 class KlearMatrix_Model_Field_Picker_Time extends KlearMatrix_Model_Field_Picker_Abstract
 {
     protected $_config;
@@ -51,7 +50,7 @@ class KlearMatrix_Model_Field_Picker_Time extends KlearMatrix_Model_Field_Picker
         return 'H:i:s';
     }
 
-    public function getFormat()
+    public function getFormat($locale = null)
     {
         return 'hh:mm:ss';
     }

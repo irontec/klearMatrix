3d6990e735e46bf8012b65109502f22d9f816220
Alayn Gortazar <zutoin@gmail.com>
2012-07-25 09:04:39 +0000

    Corregida indentación
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@261 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- Bootstrap.php
+++ Bootstrap.php
@@ -18,18 +18,18 @@ class KlearMatrix_Bootstrap extends Zend_Application_Module_Bootstrap
     protected function _initAutoload()
     {
         $autoloader = new Zend_Application_Module_Autoloader(
-                array(
-                        'namespace' => 'KlearMatrix',
-                        'basePath'  => __DIR__,
-                )
+            array(
+                'namespace' => 'KlearMatrix',
+                'basePath'  => __DIR__,
+            )
         );
 
         $autoloader->addResourceType('actionhelpers', 'controllers/helpers/', 'Controller_Helper');
         $autoloader->addResourceType('exceptions', 'exceptions/', 'Exception');
 
         Zend_Controller_Action_HelperBroker::addPath(
-                __DIR__ . '/controllers/helpers',
-                'KlearMatrix_Controller_Helper_'
+            __DIR__ . '/controllers/helpers',
+            'KlearMatrix_Controller_Helper_'
         );
 
         return $autoloader;

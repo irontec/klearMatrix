653888e8a4414a335025b0ae0079bdf8c2f50456
Alayn Gortazar <zutoin@gmail.com>
2012-07-30 10:41:29 +0000

    TinyMCE funcionando mejor. Se comprueba que el plugin esté cargado antes de invocarlo.
    Ya funciona en campos multilang también
    
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@265 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- assets/js/plugins/jquery.ui.kleartinymce.js
+++ assets/js/plugins/jquery.ui.kleartinymce.js
@@ -1,47 +1,49 @@
-(function($, undefined) {
-	
-	$.widget('ui.kleartinymce', {
-		options: {
-		},
-		_init:function(){
-			
-			var $el = $(this.element);
-			
-			var options = this.options;
-			
-			var _self = this;
-			
-			var _contentChange = function(instance) {
-				var changed = !(instance.startContent == instance.getBody().innerHTML); 
-				if (changed) {
-					$("#"+instance.editorId).trigger('manualchange');
-				}
-			};
-			
-			var tinySettings = {
-				onchange_callback : function(instance) {
-					_contentChange(instance);
-				},
-				handle_event_callback :function (event, instance) {
-					switch (event.type) {
-						case 'keyup':
-						case 'click':
-							_contentChange(instance);
-							break;
-					}
-					return true;
-				},
-				init_instance_callback : function (instance) {
-					
-				}
-			};
-			
-			$.extend(tinySettings, options);
-			
-			
-			$el.tinymce(tinySettings);
-			
-		}
-			
-	});
-})(jQuery);	
+(function load($, undefined) {
+    if (!$.klear.checkDeps(['$.fn.tinymce', 'window.tinymce'], load)) {
+        return;
+    }
+
+    $.widget('ui.kleartinymce', {
+        options: {
+        },
+        _init:function(){
+            
+            var $el = $(this.element);
+            
+            var options = this.options;
+            
+            var _self = this;
+            
+            var _contentChange = function(instance) {
+                var changed = !(instance.startContent == instance.getBody().innerHTML); 
+                if (changed) {
+                    $("#"+instance.editorId).trigger('manualchange');
+                }
+            };
+            
+            var tinySettings = {
+                onchange_callback : function(instance) {
+                    _contentChange(instance);
+                },
+                handle_event_callback :function (event, instance) {
+                    switch (event.type) {
+                        case 'keyup':
+                        case 'click':
+                            _contentChange(instance);
+                            break;
+                    }
+                    return true;
+                },
+                init_instance_callback : function (instance) {
+                    
+                }
+            };
+            
+            $.extend(tinySettings, options);
+            
+            $el.tinymce(tinySettings);
+            
+        }
+            
+    });
+})(jQuery);

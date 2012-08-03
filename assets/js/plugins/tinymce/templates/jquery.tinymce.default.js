f255c3163b97442da0829df51cb2d06a5fb9b18f
Alayn Gortazar <zutoin@gmail.com>
2012-08-03 08:38:26 +0000

    Añadido sistema para templates de tinymce. No es funcional todavía, cuando Lander saque un rato, lo prepara.
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@270 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

---
+++ assets/js/plugins/tinymce/templates/jquery.tinymce.default.js
@@ -0,0 +1,13 @@
+(function($, undefined) {
+
+    $.klear.tinyKlearTemplate = $.klear.tinyKlearTemplate || {};
+    
+    $.klear.tinyKlearTemplate['default'] = function(options) {
+        /*var options = options;
+        this.settings = "22";
+        this.getSettings = function (){
+            return this.settings;
+        }*/
+    };
+    
+})(jQuery);    

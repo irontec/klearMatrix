6dcb3047a3998cd7ccf9e16b8ba3ece5cecba3bb
David Lores <david@irontec.com>
2012-09-12 16:30:05 +0000

    #0026057 En los select de tipo mapper ahora es posible añadir un visualFilter (dentro del config), indicando un campo del mapper, y por cada valor devuelto un show y hide. Si el select tiene null se puede añadir también
    config:
     visualFilter:
     hasMobile:
       'null':
         show: []
         hide: [defaultDestinationUserPhoneType]
       0:
         show: []
         hide: [defaultDestinationUserPhoneType]
       1:
         show: [defaultDestinationUserPhoneType]
         hide: []
    
    
    
    
    
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@281 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Select/Mapper.php
+++ models/Field/Select/Mapper.php
@@ -2,11 +2,15 @@
 
 class KlearMatrix_Model_Field_Select_Mapper extends KlearMatrix_Model_Field_Select_Abstract
 {
+    protected $_showOnSelect = array();
+    protected $_hideOnSelect = array();
 
     public function init()
     {
         $_mapper = $this->_config->getProperty("config")->mapperName;
         $_fieldConf = $this->_config->getProperty('config')->fieldName;
+
+        $visualFilter = $this->_config->getProperty('config')->visualFilter;
 
         $_where = null;
 
@@ -54,8 +58,50 @@ class KlearMatrix_Model_Field_Select_Mapper extends KlearMatrix_Model_Field_Sele
 
                 $this->_items[] = str_replace(array_keys($replace), $replace, $fieldTemplate);
                 $this->_keys[] = $dataModel->getPrimaryKey();
+
+                if ($visualFilter) {
+
+                    foreach ($visualFilter as $key => $config) {
+
+                        if ($this->_config->getProperty("null")) {
+
+                            if ($config->null) {
+
+                                $this->_showOnSelect['__null__'] = $config->null->show;
+                                $this->_hideOnSelect['__null__'] = $config->null->hide;
+
+                            } else {
+
+                                $this->_showOnSelect['__null__'] = array();
+                                $this->_hideOnSelect['__null__'] = array();
+                            }
+                        }
+
+                        $_getter = 'get' . $dataModel->columnNameToVar($key);
+                        $value = $dataModel->$_getter();
+
+                        if ($config->$value) {
+
+                            $this->_showOnSelect[$dataModel->getPrimaryKey()] = $config->$value->show;
+                            $this->_hideOnSelect[$dataModel->getPrimaryKey()] = $config->$value->hide;
+                        }
+                    }
+                }
             }
         }
+    }
+
+    public function getExtraConfigArray()
+    {
+        $ret = array();
+
+        if (sizeof($this->_showOnSelect)>0 || sizeof($this->_hideOnSelect)>0) {
+
+            $ret['visualFilter']['show'] = (array)$this->_showOnSelect;
+            $ret['visualFilter']['hide'] = (array)$this->_hideOnSelect;
+        }
+
+        return $ret;
     }
 }
 

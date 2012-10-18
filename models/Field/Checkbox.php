f7d75d026b940df803f769a12aab458ca16c879c
Alayn Gortazar <zutoin@gmail.com>
2012-10-18 11:34:05 +0000

    Magia para mostrar un error algo más intuitivo cuando falla un Yaml. Con anonymous functions y tol tema.
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@340 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

---
+++ models/Field/Checkbox.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Clase que devuelve la ruta al forward de _dispatch en base a la configuración a los parámetros de request
+* @author jabi
+*
+*/
+
+class KlearMatrix_Model_Field_Checkbox extends KlearMatrix_Model_Field_Abstract
+{
+    protected $_adapter;
+
+    public function init()
+    {
+        parent::init();
+        $sourceConfig = $this->_config->getRaw()->source;
+
+//         $adapterClassName = "KlearMatrix_Model_Field_CheckBox_" . ucfirst($sourceConfig->data);
+
+        $this->_adapter = new $adapterClassName;
+        $this->_adapter
+                    ->setConfig($sourceConfig)
+                    ->setColumn($this->_column)
+                    ->init();
+    }
+
+    public function filterValue($value, $original)
+    {
+        if ($value == '__NULL__') {
+
+            return NULL;
+        }
+
+        return $value;
+    }
+
+    public function getConfig()
+    {
+        $ret = array_merge(
+            $this->_adapter->getExtraConfigArray(),
+            array('values' => $this->_adapter->toArray())
+        );
+
+        return $ret;
+    }
+
+}
+
+//EOF

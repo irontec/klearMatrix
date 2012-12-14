9a4a3283e0a95623bc629d89617449838991d481
Mikel Madariaga <mikel@irontec.com>
2012-12-14 11:47:09 +0000

    Classes pendiente de parentOptionCustomizers
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@408 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

---
+++ models/ParentOptionCustomizer/RecordCount.php
@@ -0,0 +1,68 @@
+<?php
+class KlearMatrix_Model_ParentOptionCustomizer_RecordCount implements KlearMatrix_Model_Interfaces_ParentOptionCustomizer
+{
+    /**
+     * @var KlearMatrix_Model_RouteDispatcher
+     */
+    protected $_mainRouter = null;
+
+    /**
+     * @var array
+     */
+    protected $_mainRouterOriginalParams = null;
+
+    /**
+     * @var KlearMatrix_Controller_Helper_CreateListWhere
+     */
+    protected $_createListWhere = null;
+
+    /**
+     * @var KlearMatrix_Model_AbstractOption
+     */
+    protected $_option = null;
+
+    protected $_resultWrapper = 'span';
+    protected $_cssClass = 'recordCount';
+
+    public function __construct()
+    {
+        $this->_createListWhere = new KlearMatrix_Controller_Helper_CreateListWhere;
+
+        $front = Zend_Controller_Front::getInstance();
+        $this->_mainRouter = $front->getRequest()->getUserParam("mainRouter");
+        $this->_mainRouterOriginalParams = $this->_mainRouter ->getParams();
+    }
+
+    public function setOption (KlearMatrix_Model_AbstractOption $option) {
+
+        $this->_option = $option;
+    }
+
+    /**
+     * @return KlearMatrix_Model_ParentOptionCustomizer_Response
+     */
+    public function customize($parentModel)
+    {
+        $item = $this->_mainRouter->loadScreen($this->_option->getName());
+        $model = $item->getObjectInstance();
+        $mapper = $model->getMapper();
+
+        //Al tratarse de un filtered screen necesita la pk del padre
+        $this->_mainRouter->setParams(array("pk" => $parentModel->getPrimaryKey()));
+
+        $where = $this->_createListWhere->createListWhere(new KlearMatrix_Model_ColumnCollection(),
+                                                             $model,
+                                                          new KlearMatrix_Model_MatrixResponse(),
+                                                          $item);
+
+        $this->_mainRouter->setParams($this->_mainRouterOriginalParams);
+
+        $response = new KlearMatrix_Model_ParentOptionCustomizer_Response();
+        $response->setResult($mapper->countByQuery($where))
+                 ->setWrapper($this->_resultWrapper)
+                 ->setCssClass($this->_cssClass);
+
+        return $response;
+    }
+
+}
---
+++ models/ParentOptionCustomizer/Response.php
@@ -0,0 +1,51 @@
+<?php
+class KlearMatrix_Model_ParentOptionCustomizer_Response {
+
+    protected $_result;
+    protected $_wrapper;
+    protected $_cssClass;
+
+    public function __contruct() {}
+
+    public function setResult($data) {
+
+        $this->_result = $data;
+        return $this;
+    }
+
+    public function getResult() {
+
+        return $this->_result;
+    }
+
+    public function setWrapper($data) {
+
+        $this->_wrapper = $data;
+        return $this;
+    }
+
+    public function getWrapper() {
+
+        return $this->_wrapper;
+    }
+
+    public function setCssClass($data) {
+
+        $this->_cssClass = $data;
+        return $this;
+    }
+
+    public function getCssClass() {
+
+        return $this->_cssClass;
+    }
+
+    public function toArray() {
+
+        return array(
+            'result'  => $this->_result,
+            'wrapper' => $this->_wrapper,
+            'class'   => $this->_cssClass,
+        );
+    }
+}

b79df6e292fbc339cd273a20dd8aaec9d640c785
Alayn Gortazar <zutoin@gmail.com>
2013-01-21 14:16:30 +0000

    Cambios en Field Select_Mapper: A partir de ahora es OBLIGATORIO que una clase de filtro para las select implemente KlearMatrix_Model_Field_Select_Filter_Interface
    Además no es necesario que setRouteDispatcher devuelva true para que getCondition se ejecute. La comprobación de si hay condición o no irá directamente en getCondition().
    
    La forma en la que se hacía antes era muy poco intuitiva para programadores que necesitasen usar la interfaz.
    
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@448 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Select/Filter/Interface.php
+++ models/Field/Select/Filter/Interface.php
@@ -3,5 +3,10 @@
 interface KlearMatrix_Model_Field_Select_Filter_Interface
 {
     public function setRouteDispatcher(KlearMatrix_Model_RouteDispatcher $routeDispatcher);
+
+    /**
+     * Returns where condition for select field mapper. If no condition is needed for current object, return NULL
+     * @return string|NULL
+     */
     public function getCondition();
 }

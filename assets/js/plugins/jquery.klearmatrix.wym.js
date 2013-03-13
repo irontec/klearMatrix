739580e26c74c16a2cdd40f0f0f37ba776209e8f
Dani Rendon <ddniel16@gmail.com>
2013-03-13 15:09:10 +0000

    opcion "buttons" para el editor wym
    buttons:
      - Bold
      ...
      
    haci solo se mostraran los botones de esta lista.
    
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@510 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- assets/js/plugins/jquery.klearmatrix.wym.js
+++ assets/js/plugins/jquery.klearmatrix.wym.js
@@ -13,9 +13,9 @@
 
     $.widget('klearmatrix.wym', {
         options: {},
-
+        
         _init:function() {
-
+            
             var availableSettings = {
                 "_contentTab": true,
                 "lang" : true,
@@ -27,29 +27,11 @@
                 "stylesheet": true,
                 "plugins" : true,
             };
-
+            
             var editorOptions = {
-
-                toolsItems: [
-                    {'name': 'Bold', 'title': 'Strong', 'css': 'wym_tools_strong'},
-                    {'name': 'Italic', 'title': 'Emphasis', 'css': 'wym_tools_emphasis'},
-                    {'name': 'Superscript', 'title': 'Superscript', 'css': 'wym_tools_superscript'},
-                    {'name': 'Subscript', 'title': 'Subscript', 'css': 'wym_tools_subscript'},
-                    {'name': 'InsertOrderedList', 'title': 'Ordered_List', 'css': 'wym_tools_ordered_list'},
-                    {'name': 'InsertUnorderedList', 'title': 'Unordered_List', 'css': 'wym_tools_unordered_list'},
-                    {'name': 'Indent', 'title': 'Indent', 'css': 'wym_tools_indent'},
-                    {'name': 'Outdent', 'title': 'Outdent', 'css': 'wym_tools_outdent'},
-                    {'name': 'Undo', 'title': 'Undo', 'css': 'wym_tools_undo'},
-                    {'name': 'Redo', 'title': 'Redo', 'css': 'wym_tools_redo'},
-                    {'name': 'CreateLink', 'title': 'Link', 'css': 'wym_tools_link'},
-                    {'name': 'Unlink', 'title': 'Unlink', 'css': 'wym_tools_unlink'},
-                    {'name': 'InsertTable', 'title': 'Table', 'css': 'wym_tools_table'},
-                    {'name': 'Paste', 'title': 'Paste_From_Word', 'css': 'wym_tools_paste'},
-                    {'name': 'ToggleHtml', 'title': 'HTML', 'css': 'wym_tools_html'},
-                    {'name': 'Preview', 'title': 'Preview', 'css': 'wym_tools_preview'}
-                ],
+                toolsItems: this._getButtons(),
                 postInit: function(wym) {
-
+                    
                     //postInit is executed after WYMeditor initialization
                     //'wym' is the current WYMeditor instance
 
@@ -74,7 +56,7 @@
                     }
                 }
             };
-
+            
             for (idx in this.options) {
 
                 if(availableSettings[idx]) {
@@ -161,7 +143,58 @@
                 return;
             }
         },
-
+        
+        _getButtons: function () {
+            
+            if (this.options.buttons) {
+                var buttons = this.options.buttons.split(',');
+            } 
+            
+            var baseArray = [
+             {'name': 'Bold', 'title': 'Strong', 'css': 'wym_tools_strong'},
+             {'name': 'Italic', 'title': 'Emphasis', 'css': 'wym_tools_emphasis'},
+             {'name': 'Superscript', 'title': 'Superscript', 'css': 'wym_tools_superscript'},
+             {'name': 'Subscript', 'title': 'Subscript', 'css': 'wym_tools_subscript'},
+             {'name': 'InsertOrderedList', 'title': 'Ordered_List', 'css': 'wym_tools_ordered_list'},
+             {'name': 'InsertUnorderedList', 'title': 'Unordered_List', 'css': 'wym_tools_unordered_list'},
+             {'name': 'Indent', 'title': 'Indent', 'css': 'wym_tools_indent'},
+             {'name': 'Outdent', 'title': 'Outdent', 'css': 'wym_tools_outdent'},
+             {'name': 'Undo', 'title': 'Undo', 'css': 'wym_tools_undo'},
+             {'name': 'Redo', 'title': 'Redo', 'css': 'wym_tools_redo'},
+             {'name': 'CreateLink', 'title': 'Link', 'css': 'wym_tools_link'},
+             {'name': 'Unlink', 'title': 'Unlink', 'css': 'wym_tools_unlink'},
+             {'name': 'InsertTable', 'title': 'Table', 'css': 'wym_tools_table'},
+             {'name': 'Paste', 'title': 'Paste_From_Word', 'css': 'wym_tools_paste'},
+             {'name': 'ToggleHtml', 'title': 'HTML', 'css': 'wym_tools_html'},
+             {'name': 'Preview', 'title': 'Preview', 'css': 'wym_tools_preview'},
+             ];
+            
+            if (buttons) {
+                
+                var buttonIndex = {};
+                
+                for (var i in baseArray) {
+                    
+                    buttonIndex[baseArray[i]['name']] = i;
+                }
+                
+                var retArray = [];
+                
+                for (var i=0;i<buttons.length;i++) {
+                    var curButton = $.trim(buttons[i]);
+                    if (buttonIndex[curButton]) {
+                        
+                        retArray.push(baseArray[buttonIndex[curButton]]);
+                    }
+                    
+                }
+                return retArray;
+                
+            } else {
+                return baseArray;
+            }
+        },
+        
         _isMultilang: function ($el) {
 
             return $el.parent().prop("tagName").toLowerCase() == "dd";

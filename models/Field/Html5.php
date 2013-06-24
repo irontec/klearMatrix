359b00912732dff9ca84832f83ecd5285c7a240a
Alayn Gortazar <zutoin@gmail.com>
2013-06-24 08:38:43 +0000

    #0032407: Pruebas con jenkins Volviendo a poner bien los estilos
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@611 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Html5.php
+++ models/Field/Html5.php
@@ -1,26 +1,27 @@
 <?php
+
 class KlearMatrix_Model_Field_Html5 extends KlearMatrix_Model_Field_Abstract
 {
     protected $_adapter;
-    
+
     protected function _init()
     {
         $sourceConfig = $this->_config->getRaw()->source;
-    
+
         if ($sourceConfig) {
-            
+
             $controlClassName = "KlearMatrix_Model_Field_Html5_" . ucfirst($sourceConfig->control);
             $this->_adapter = new $controlClassName($sourceConfig);
-            
+
             $this->_js = $this->_adapter->getExtraJavascript();
             $this->_css = $this->_adapter->getExtraCss();
         }
     }
-    
+
     public function filterValue($value)
     {
-        
+
         return $this->_adapter->filterValue($value);
-        
+
     }
 }

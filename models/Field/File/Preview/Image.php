5b6c32beb8b1656d258c71f327bee12eaa997b03
David Lores <david@irontec.com>
2014-01-28 12:34:47 +0000

    #0036920 En preview de imagenes, por defecto crop es false y width/height es 0.
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@739 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/File/Preview/Image.php
+++ models/Field/File/Preview/Image.php
@@ -1,21 +1,18 @@
 <?php
 
-class KlearMatrix_Model_Field_File_Preview_Image
-    implements KlearMatrix_Model_Field_File_Preview_Interface
+class KlearMatrix_Model_Field_File_Preview_Image implements KlearMatrix_Model_Field_File_Preview_Interface
 {
-
     protected $_width;
     protected $_height;
-    protected $_crop = true;
+    protected $_crop = false;
     protected $_binary;
-
 
     public function setRequest(Zend_Controller_Request_Http $request)
     {
-        $this->_width = $request->getParam('width', '200');
-        $this->_height = $request->getParam('height', '200');
-        if ($request->getParam('crop', 'true') == 'false') {
-            $this->_crop = false;
+        $this->_width = $request->getParam('width', '0');
+        $this->_height = $request->getParam('height', '0');
+        if ($request->getParam('crop', 'false') == 'true') {
+            $this->_crop = true;
         }
         return;
     }
@@ -37,5 +34,4 @@ class KlearMatrix_Model_Field_File_Preview_Image
     {
         return $this->_binary;
     }
-
 }

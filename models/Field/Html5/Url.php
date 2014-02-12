d08ba20758a2f700ea21e45034c97b2d411c3d98
Javier Infante <jabi+github@irontec.com>
2014-02-12 18:44:43 +0000

    Solucionado el commit anterior. el filterValue ya no da SIEMPRE un Notice. #37383
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@753 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Html5/Url.php
+++ models/Field/Html5/Url.php
@@ -9,18 +9,34 @@ class KlearMatrix_Model_Field_Html5_Url extends KlearMatrix_Model_Field_Html5_Ab
         );
     }
 
+	protected function _validateUrl($value)
+	{
+		if (empty($value)) {
+			return '';
+		}
+		
+		if (false === filter_var($value, FILTER_VALIDATE_URL)) {
+			$translator = Zend_Registry::get(Klear_Plugin_Translator::DEFAULT_REGISTRY_KEY);
+			throw new Klear_Exception_Default($translator->translate("Field is not a valid URL."));
+		}
+		
+		return $value;
+	}
+	
+	
+    
+    
     public function filterValue($value)
     {
-        if (!is_array($value)) 
-            $value = array($value);
-
-        if ($value != ""
-            && false === filter_var($value, FILTER_VALIDATE_URL)) {
-            $translator = Zend_Registry::get(Klear_Plugin_Translator::DEFAULT_REGISTRY_KEY);
-            throw new Klear_Exception_Default($translator->translate("Field is not a valid URL."));
+    	
+        if (!is_array($value)) {
+        	return $this->_validateUrl($value);
         }
 
+        foreach ($value as $idx => $_val) {
+        	$value[$idx] = $this->_validateUrl($_val);
+        }
+        
         return $value;
-
     }
 }

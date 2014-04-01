65bd3359c76f69222a8312956c7aa13e0fd7ec2a
Javier Infante <jabi+github@irontec.com>
2014-04-01 17:55:38 +0000

    Se actualiza la versión de tinyMCE directamente desde su master.
    Se crea un mini howto:
    assets/js/plugins/tinymce/jscripts/tinymce/HOWTO_UPADTE_tinyMce.txt
    
    Se apunta a tinymce.full.min.js (un bundle con la mayoria de plugins+themes)
    
    En principio parece que se soluciona el error (aunque no estaba muy modelizado)
    
    #38734
    
    
    
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@809 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Textarea/Tinymce.php
+++ models/Field/Textarea/Tinymce.php
@@ -49,7 +49,9 @@ class KlearMatrix_Model_Field_Textarea_Tinymce extends KlearMatrix_Model_Field_T
         $this->_settings['language'] = $this->_lang->getLanguage();
 
         // Location of TinyMCE script
-        $this->_settings['script_url'] = $this->_jsPluginPath . '/tinymce.min.js';
+        // We load tinymce.full.min (most of plugins + templete in the bundle)
+        //$this->_settings['script_url'] = $this->_jsPluginPath . '/tinymce.min.js';
+        $this->_settings['script_url'] = $this->_jsPluginPath . '/tinymce.full.min.js';
         $this->_settings = array_merge($this->_settings, $this->_template->getSettings());
     }
 

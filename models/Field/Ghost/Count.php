aeee2f97aae00d0dc3094cfe3d1d5938f2f5114d
Luis Felipe García <lfgarcia@irontec.com>
2014-06-19 06:52:47 +0000

    #40583 Limpieza de código en Ghost count (eliminados comentarios)
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@883 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Ghost/Count.php
+++ models/Field/Ghost/Count.php
@@ -25,10 +25,6 @@ class KlearMatrix_Model_Field_Ghost_Count extends KlearMatrix_Model_Field_Ghost_
         $this->_parentField->setSearchMethod('getSearch');
         $this->_parentField->setOrderMethod('getOrder');
         $this->_parentField->setGetterMethod('getValue');
-
-        // por definición toda las columnas de Concat serán dirty (nos ahorramos ponerlo, y habrá HTML casi siempre)
-//         $this->_parentField->getColumn()->markAsDirty();
-
         return $this;
     }
 
@@ -39,56 +35,7 @@ class KlearMatrix_Model_Field_Ghost_Count extends KlearMatrix_Model_Field_Ghost_
         }
 
         $mainModel = $this->_parentField->getColumn()->getModel();
-
-//         foreach ($this->_config->getRaw()->source->template as $field => $fConfig) {
-//             $curField = array();
-//             $curField['getter'] = 'get' . $mainModel->columnNameToVar($field);
-//             if (is_string($fConfig)) {
-//                 $curField['literal'] =  Klear_Model_Gettext::gettextCheck($fConfig);
-//             } else {
-//                 if (isset($fConfig->literal)) {
-//                     $curField['literal'] =  Klear_Model_Gettext::gettextCheck($fConfig->literal);
-//                 } else {
-//                     $curField['literal'] = '%' . $field . '%';
-//                 }
-
-//                 if (isset($fConfig->checkEmpty)) {
-//                     $curField['checkEmpty'] = (bool)$fConfig->checkEmpty;
-//                 }
-
-//                 if (isset($fConfig->noSearch)) {
-//                     $curField['noSearch'] = (bool)$fConfig->noSearch;
-//                 }
-//             }
-
-//             $this->_templateFields[$field] = $curField;
-
-//         }
-//         $this->_templateFields["name"] = array(
-//         		"getter" => "getName",
-//         		"literal" => "hola"
-//         		);
-
     }
-
-//     protected function _highlightFoundString($returnStr)
-//     {
-//         if (sizeof($this->_searchedValues) == 0) {
-//             return $returnStr;
-//         }
-
-//         foreach ($this->_searchedValues as $value) {
-//             $returnStr = preg_replace(
-//                 '/('.preg_quote(trim($value)).')(?=[^><]*<|.$)/i',
-//                 '<span class="ui-state-highlight">\1</span>',
-//                 $returnStr
-//             );
-//         }
-//         return $returnStr;
-
-
-
-//     }
 
     public function getValue($model)
     {
@@ -117,7 +64,6 @@ class KlearMatrix_Model_Field_Ghost_Count extends KlearMatrix_Model_Field_Ghost_
         }
         $mapper = new $mapperName();
         $nCount = $mapper->countByQuery($countWhere);
-
         if ($totalCondition = $this->_config->getRaw()->source->totalCondition){
         	$pattern = "|%(.*)%|U";
         	if (preg_match_all($pattern, $totalCondition, $matches)){
@@ -135,81 +81,12 @@ class KlearMatrix_Model_Field_Ghost_Count extends KlearMatrix_Model_Field_Ghost_
 				$template = str_replace("%total%", $nTotal, $template);
 				$template = str_replace("%percent%", $percent, $template);
 				$returnStr = $template;
-        	} else { //'%count% / %total% (%percent%%)'
+        	} else {
         		$returnStr = $nCount." / ".$nTotal." (".$percent."%)";
         	}
         } else {
         	$returnStr = $nCount;
         }
-
-
-
-
-
-
-
-//         foreach ($this->_templateFields as $field => $fConfig) {
-
-//             $value = $model->{$fConfig['getter']}();
-
-//             if (isset($fConfig['checkEmpty'])) {
-//                 if (empty($value)) {
-//                     continue;
-//                 }
-//             }
-
-//             $returnStr .= str_replace('%' . $field . '%', $value, $fConfig['literal']);
-
-//         }
-//         return $this->_highlightFoundString($returnStr);
 		return $returnStr;
     }
-
-
-//     public function getSearch($values, $searchOps, $model)
-//     {
-//         $searchOps; // Avoid PMD UnusedLocalVariable warning
-//         $model; // Avoid PMD UnusedLocalVariable warning
-
-//         $this->_searchedValues = $values;
-//         $masterConditions = array();
-//         $fieldValues = array();
-//         $namedParams = $this->_parentField->getColumn()->namedParamsAreSupported();
-//         $cont = 0;
-
-//         foreach ($this->_templateFields as $field => $fConfig) {
-//             $auxCondition = array();
-//             if (isset($fConfig['noSearch']) &&
-//                 $fConfig['noSearch']) {
-//                 continue;
-//             }
-
-//             foreach ($values as $value) {
-//                 $template = $field . $cont++;
-//                 if ($namedParams) {
-//                     $auxCondition[] =  $field . ' like :' . $template;
-//                     $fieldValues[$template] = '%' . $value . '%';
-//                 } else {
-//                     $auxCondition[] = $field . ' like ?';
-//                     $fieldValues[] = '%' . $value . '%';
-//                 }
-//             }
-//             $masterConditions[] = '(' . implode(' or ', $auxCondition) . ')';
-//         }
-
-
-//         return array(
-//                 '(' . implode(' or ', $masterConditions). ')',
-//                 $fieldValues
-//         );
-
-//     }
-
-//     public function getOrder($model)
-//     {
-//         $model; // Avoid PMD UnusedLocalVariable warning
-//         return array_keys($this->_templateFields);
-//     }
-
-
 }

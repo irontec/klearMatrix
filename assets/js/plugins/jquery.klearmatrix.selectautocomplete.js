21a59994a39292c87987f8504f2226c7c14e6183
David Lores <david@irontec.com>
2014-06-24 10:41:59 +0000

    #0040741 En el autocomplete, al escribir un texto sin resultados borra todo. Modificado para que solo lo borre al perder el foco
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@889 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- assets/js/plugins/jquery.klearmatrix.selectautocomplete.js
+++ assets/js/plugins/jquery.klearmatrix.selectautocomplete.js
@@ -1,60 +1,48 @@
 ;(function load($) {
     $.widget("klearmatrix.selectautocomplete", {
-        widgetEventPrefix:"file",
+        widgetEventPrefix: "file",
         lastCounter : 0,
         options: {
             cache: {} // cache de nodos dom
         },
-
         element : null,
         wrapper: null,
         input : null,
         select: null,
         selected: null,
         value: null,
-
         _setOption: function (name, value) {
-
             $.Widget.prototype._setOption.apply(this, arguments);
         },
-
         _create: function() {
-
-            var self = this;
         },
-
         _init: function () {
+
             var context = this.element.klearModule("getPanel");
             if (context.get(0).tagName.toLowerCase() == 'tr') {
-
                 //Estamos en un listado
                 //nothing to do here
                 return;
             }
 
             this.options.cache.dummy = context;
-
             this.options.cache.context = context.parent();
             this.options.cache.element = this.options.cache.context.find("select");
 
             if (this.options.cache.element.length > 0) {
-
                 this._initSelectedValue();
                 this._initAutocomplete();
-
             } else {
-
                 this._initFilteringForm();
             }
         },
-
         _initFilteringForm: function () {
 
             var _self = this;
             this.options.cache.context = this.options.cache.context.parent();
             this.options.cache.dummy = this.options.cache.dummy;
 
-            this.options.cache.dummy.parent().on('manualchange',function(e) {
+            this.options.cache.dummy.parent().on('manualchange', function(e) {
 
                 if (_self.options.cache.dummy.filter(":selected").length > 0) {
 
@@ -62,15 +50,11 @@
 
                     var searchOption = _self.options.cache.context.find("span.searchOption");
                     var searchField = _self.options.cache.context.find("input.term");
-                    var selectedValue = searchField.val();
                     var column = _self.options.parent.options.data.columns  ;
 
                     for (var idx in column) {
-
                         if (column[idx].config && column[idx].config.plugin) {
-
                             if(searchField[column[idx].config.plugin]) {
-
                                 searchField[column[idx].config.plugin]("destroy");
                             }
                         }
@@ -81,15 +65,15 @@
                 }
             });
         },
-
         _postManualChange: function () {
+
             var recoveredValue = this.options.cache.element.data("recoveredValue");
             if (recoveredValue && (recoveredValue !== this.options.cache.element.val())) {
                 this._initSelectedValue();
             }
         },
-
         _initSelectedValue: function () {
+
             var preloadValue = this.options.cache.element.data("preload");
             var recoveredValue = this.options.cache.element.data("recoveredValue");
 
@@ -98,7 +82,7 @@
                 var _self = this;
                 var value2load = recoveredValue || preloadValue;
 
-                if (this.options.cache.element.find("option[value"+ value2load +"]").length == 0) {
+                if (this.options.cache.element.find("option[value" + value2load + "]").length == 0) {
 
                    var targetUrl = this.options.cache.dummy.attr("href") + "&value=" + value2load;
 
@@ -111,11 +95,13 @@
 
                         _self.options.cache.element.children("[selected]").removeAttr("selected");
 
-                        for (first in data.results) break;
+                        for (first in data.results) {
+                            break;
+                        }
                         var element = data.results[first];
                         var option = $("<option>").attr("value", element.id )
-                                                  .attr("selected", "selected")
-                                                  .html(element.value);
+                        .attr("selected", "selected")
+                        .html(element.value);
 
                         option.appendTo(_self.options.cache.element);
                         _self.options.cache.element.val(element.id);
@@ -132,51 +118,47 @@
                         _self.options.cache.element.trigger("change");
                       }
                    });
-
                 }
             }
         },
-
         _initAutocomplete: function () {
+
             var _self = this;
+            this.select = this.options.cache.element.hide();
+            this.selected = this.select.children(":selected");
 
-            this.select = this.options.cache.element.hide(),
-            this.selected = this.select.children( ":selected" ),
-
-            this.value = this.selected.val() ? this.selected.text() : "",
-            this.wrapper = $("<span>").addClass( "ui-combobox" ).insertAfter( this.select );
+            this.value = this.selected.val()? this.selected.text() : "";
+            this.wrapper = $("<span>").addClass("ui-combobox").insertAfter(this.select);
             this.wrapper.before('<span class="ui-icon inline ui-icon-script"></span>');
 
-            this.input = $( "<input>" )
-                .appendTo( this.wrapper )
-                .val( this.value )
-                .attr( "title", "" )
-                .addClass("ui-state-default ui-combobox-input ui-corner-all")
-                .addClass("ui-widget ui-widget-content ui-corner-left");
+            this.input = $("<input>")
+            .appendTo(this.wrapper)
+            .val(this.value)
+            .attr("title", "")
+            .addClass("ui-state-default ui-combobox-input ui-corner-all")
+            .addClass("ui-widget ui-widget-content ui-corner-left");
 
             //Se le envía el target para el highlight y se bindea "postmanualchange"
-            this.select.data('target-for-change',this.select.next("span").children("input:eq(0)"));
+            this.select.data('target-for-change', this.select.next("span").children("input:eq(0)"));
             this.select.on('postmanualchange', function() {
                 _self._postManualChange();
             });
 
            this._initUIsAutocomplete(this.input, this);
         },
-
         _initUIsAutocomplete: function (targetNode, context) {
-            var self = this;
+
             var _self = context;
 
             targetNode.autocomplete({
                 delay: 0,
                 minLength: 0,
-                source: function( request, response ) {
+                source: function(request, response) {
 
-                    var term = request.term;
-                    $.getJSON( _self.options.cache.dummy.attr("href") , request, function( data, status, xhr ) {
+                    $.getJSON(_self.options.cache.dummy.attr("href"), request, function(data, status, xhr) {
                         _self.lastCounter = data.totalItems;
                         response(
-                            $.map( data.results, function( item ) {
+                            $.map(data.results, function(item) {
                                 return {
                                     label: item.label,
                                     value: item.value,
@@ -186,61 +168,56 @@
                         );
                     });
                 },
-
                 select: function( event, ui ) {
 
                     _self.options.cache.context.find("input.term").data("idItem", ui.item.id);
-                    var option = _self.options.cache.element.children("[value="+ ui.item.id +"]");
+                    var option = _self.options.cache.element.children("[value=" + ui.item.id + "]");
 
                     if (! option.get(0)) {
                         option = $("<option />")
-                                    .attr("value", ui.item.id )
-                                    .html(ui.item.value);
-
+                        .attr("value", ui.item.id)
+                        .html(ui.item.value);
                         option.appendTo(_self.options.cache.element);
                     }
 
                     option.get(0).selected = true;
 
-                    if(_self.select) {
+                    if (_self.select) {
                         _self.select.trigger('manualchange');
                     }
 
-                    _self._trigger( "selected", event, {
+                    _self._trigger("selected", event, {
                         item: option
                     });
                 },
                 open : function() {
-                    if ($(".autocompleteCounter",$(this).parents("span:eq(0)")).length == 0) {
+
+                    if ($(".autocompleteCounter", $(this).parents("span:eq(0)")).length == 0) {
                         $(this).parents("span:eq(0)").append('<span class="autocompleteCounter"></span>');
                     }
-                    var $counter = $(".autocompleteCounter",$(this).parents("span:eq(0)"));
+                    var $counter = $(".autocompleteCounter", $(this).parents("span:eq(0)"));
                     $counter.html(_self.lastCounter).show();
-
                 },
                 close : function(event, ui) {
-                    $(".autocompleteCounter",$(this).parents("span:eq(0)")).fadeOut('fast');
-                    if ( !ui.item ) {
-                        return _self._removeIfInvalid( this );
-                    }
-                },
-                change: function( event, ui ) {
 
-                    if ( !ui.item ) {
-                        return _self._removeIfInvalid( this );
+                    $(".autocompleteCounter", $(this).parents("span:eq(0)")).fadeOut('fast');
+                },
+                change: function(event, ui) {
+
+                    if (!ui.item) {
+                        return _self._removeIfInvalid(this);
                     }
                 }
-
             }).on("focusin", function () {
-
                 $(this).select();
             });
 
-            targetNode.data("autocomplete")._renderItem = function( ul, item ) {
+            targetNode.data("autocomplete")._renderItem = function(ul, item) {
+                var element = $("<li>").data("item.autocomplete", item)
+                .append("<a>" + item.label + "</a>")
+                .appendTo(ul);
 
-                return $( "<li>" ).data( "item.autocomplete", item )
-                                  .append( "<a>" + item.label + "</a>" )
-                                  .appendTo( ul );
+                return element;
             };
 
             targetNode.tooltip({
@@ -250,48 +227,44 @@
                 tooltipClass: "ui-state-highlight"
             });
         },
-
         _removeIfInvalid : function (element) {
-                var _self = this;
 
-                var value = $( element ).val(),
-                    matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( value ) + "$", "i" ),
-                    valid = false;
-                this.select.children( "option" ).each(function() {
-                    if ( $( this ).text().match( matcher ) ) {
-                        this.selected = valid = true;
-                        return false;
-                    }
-                });
-
-                if ( !valid ) {
-                    // remove invalid value, as it didn't match anything
-                    $( element )
-                        .val( "" )
-                        .attr( "title", value + " didn't match any item" )
-                        .tooltip( "open" );
-
-                    this.select.val('').find("option").remove();
-                    setTimeout(function() {
-                        _self.input.tooltip( "close" ).attr( "title", "" );
-                    }, 2500 );
-
-                    this.input.data( "autocomplete" ).term = "";
+            var _self = this;
+            var value = $(element).val();
+            var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex( value ) + "$", "i");
+            var valid = false;
+            this.select.children("option").each(function() {
+                if ($(this).text().match(matcher)) {
+                    this.selected = valid = true;
                     return false;
                 }
-        },
+            });
 
+            if (!valid) {
+                // remove invalid value, as it didn't match anything
+                $(element).val("")
+                .attr("title", value + " didn't match any item")
+                .tooltip("open");
+
+                this.select.val('').find("option").remove();
+                setTimeout(function() {
+                    _self.input.tooltip("close").attr("title", "");
+                }, 2500);
+
+                this.input.data("autocomplete").term = "";
+                return false;
+            }
+        },
         destroy: function() {
 
             // remove classes + data
             this.wrapper.remove();
             this.options.cache.element.show();
-
-            $.Widget.prototype.destroy.call( this );
+            $.Widget.prototype.destroy.call(this);
             return this;
         }
     });
 
     $.widget.bridge("klearmatrix.selectautocomplete");
 
-})( jQuery );
+})(jQuery);

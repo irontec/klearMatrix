6804821e751ad880682c12b86f65fb27fd087e4a
Luis Felipe García <lfgarcia@irontec.com>
2014-07-08 10:31:51 +0000

    #41054 funcionalidad dashboard creada
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@899 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- controllers/GooglechartsController.php
+++ controllers/GooglechartsController.php
@@ -106,7 +106,14 @@ class KlearMatrix_GooglechartsController extends Zend_Controller_Action
     	->setPK($pk)
     	->setResponseItem($this->_item);
 
+
     	$chartData = array();
+		$dashboard = $this->_item->getRawConfigAttribute("dashboard");
+    	if ( $dashboard && $dashboard->show !== false){
+    		$chartData["dashboard"] = $this->_dashboard($dashboard);
+//     		var_dump($chartData);
+//     		die(1);
+    	}
     	$chartData["title"] = $screenTitle;
     	$chartData["chartGroups"] = array();
     	$chartGroups = $this->_item->getRawConfigAttribute("chartGroups");
@@ -218,6 +225,9 @@ class KlearMatrix_GooglechartsController extends Zend_Controller_Action
 		    								foreach($row as $field){
 		    									if (is_numeric($field)){
 		    										$field = floatval($field);
+// 		    									} else if (DateTime::createFromFormat('Y-m-d G:i:s', $field) !== FALSE){
+// 		    										$date = new DateTime($field);
+// 													$field = $date->format('Y,m,d,G,i,s');
 		    									} else {
 		    										$field = $this->_helper->translate($field);
 		    									}
@@ -289,183 +299,116 @@ class KlearMatrix_GooglechartsController extends Zend_Controller_Action
     	$jsonResponse->setData($this->_helper->hookedDataForScreen($this->_item, 'setData', $data));
     	$jsonResponse->attachView($this->_helper->hookedDataForScreen($this->_item, 'attachView', $this->view));
 
+    }
 
+    protected function _dashboard($config){
+    	$data['title'] = $config->title;
 
+    	if ($config->useExplain) {
+    		$data['title'] .= ' ' . $this->_helper->translate('(Aproximated values)');
+    	}
 
+    	$sectionsBlackList = array();
+    	if ($config->sectionsBlackList) {
+    		$sectionsBlackList = $config->sectionsBlackList;
+    		$sectionsBlackList = $sectionsBlackList->toArray();
+    	}
 
+    	$menuConfig = Zend_Controller_Front::getInstance()
+    	->getParam('bootstrap')
+    	->getResource('modules')
+    	->offsetGet('klear')
+    	->getOption('menu');
 
+    	$data['sections'] = array();
 
+    	foreach ($menuConfig as $section) {
+    		$sectionTmp = array(
+    				'name' => $section->getName(),
+    				'subsects' => array()
+    		);
 
+    		foreach ($section as $subsection) {
 
+    			$file = $subsection->getMainFile();
 
+    			$sectionConfig = new Klear_Model_SectionConfig;
+    			$sectionConfig->setFile($file);
+    			if (!$sectionConfig->isValid()) {
+    				continue;
+    				return;
+    			}
 
+    			if (in_array($file, $sectionsBlackList)) {
+    				continue;
+    			}
 
+    			// Nos devuelve el configurador del módulo concreto instanciado.
+    			$moduleConfig = $sectionConfig->factoryModuleConfig();
+    			$moduleRouter = $moduleConfig->buildRouterConfig();
+    			$moduleRouter->resolveDispatch();
 
+    			if ($moduleRouter->getCurrentItem()->getRawConfigAttribute("dashboard->class")) {
 
+    				$dashElementClassName = $moduleRouter->getCurrentItem()->getRawConfigAttribute("dashboard->class");
+    				$dashSection = new $dashElementClassName;
+    				$dashSection->setConfig($moduleRouter->getCurrentItem()->getRawConfigAttribute("dashboard"));
+    				$dashSection->setItem($moduleRouter->getCurrentItem());
+    				$sectionTmp['subsects'][] = array(
+    						'name' => $dashSection->getName(),
+    						'class' => $dashSection->getClass(),
+    						'file' => $dashSection->getFile(),
+    						'subtitle' => $dashSection->getSubTitle()
+    				);
 
+    				continue;
+    			}
 
+    			/*
+    			 * Para KlearMatrix List, se calcula automáticamente.
+    			*/
+    			if (($moduleRouter->getModuleName() == "klearMatrix") &&
+    					($moduleRouter->getControllerName() == "list") ) {
+    				$sectionTmp['subsects'][] = $this->_calculateForKlearMatrixList($moduleRouter, $subsection, $config);
+    				continue;
+    			}
+    		}
 
+    		if (sizeof($sectionTmp['subsects'])>0) {
+    			$data['sections'][] = $sectionTmp;
+    		}
 
+    	}
+    	return $data;
+    }
+    protected function _calculateForKlearMatrixList($moduleRouter, $subsection, $config)
+    {
+    	$_item = $moduleRouter->getCurrentItem();
 
+    	$_mapper = \KlearMatrix_Model_Mapper_Factory::create($_item->getMapperName());
 
+    	//$cols = $_item->getVisibleColumns();
+    	$model = $_item->getObjectInstance();
+    	$fakeData = new KlearMatrix_Model_MatrixResponse();
 
+    	/**
+    	 * El primer paramétro de createListWhere solamente se usa para construir
+    	 * la condición para filtrar resultados en ListControllers
+    	 */
+    	$where = $this->_helper->createListWhere(new KlearMatrix_Model_ColumnCollection(), $model, $fakeData, $_item);
 
+    	if (!$where) {
+    		$totalItems = $_mapper->countAllRows($config->useExplain);
+    	} else {
+    		$totalItems = $_mapper->countByQuery($where);
+    	}
 
+    	return array(
+    			'name' => $subsection->getName(),
+    			'class' => $subsection->getClass(),
+    			'file' => $subsection->getMainFile(),
+    			'subtitle' => $totalItems
+    	);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    	/*
-        // Tenemos el $this->_mainRouter para explotar
-        // los datos necesarios
-        // Do stuff
-        $pk = $this->getParam("pk");
-
-
-
-        $data = array();
-
-
-        $data['title'] = Klear_Model_Gettext::gettextCheck($this->_item->getTitle());
-
-        $data['parentIden'] = 'asdg';
-
-		//Generar gráficos
-        $data["chartGroups"] = array();
-        $chartGroups = $this->_item->getRawConfigAttribute("chartGroups");
-        foreach ($chartGroups as $chartGroup){
-        	if(!is_null($chartGroup->show)){
-        		$showGroup = $chartGroup->show;
-        	} else {
-        		$showGroup = true;
-        	}
-        	if($showGroup){
-        		$group = array();
-        		if(isset($chartGroup->title)){
-        			$title = Klear_Model_Gettext::gettextCheck($chartGroup->title);
-        			$group["title"] = $title;
-        		}
-        		if(isset($chartGroup->comment)){
-        			$group["comment"] = Klear_Model_Gettext::gettextCheck($chartGroup->comment);
-        		}
-
-        		//$data["chartGroups"][$title] = array();
-        		$group["charts"] = array();
-        		if(isset($chartGroup->charts)){
-	        		foreach ($chartGroup->charts as $key => $chart) {
-	        			if(isset($chart->show)){
-	        				$show = $chart->show;
-	        			} else {
-	        				$show = true;
-	        			}
-
-	        			if($show){
-	        				$chartTitle = Klear_Model_Gettext::gettextCheck($chart->title);
-							if(preg_match("/= *{parent}/", $chart->sql)){
-								if(!is_null($pk)){
-									$configSQL = preg_replace("/= *{parent}/","= ". $pk, $chart->sql);
-								} else {
-									$configSQL = preg_replace("/= *{parent}/"," IS NOT NULL", $chart->sql);
-								}
-							} else {
-								$configSQL = $chart->sql;
-							}
-							$sql = Klear_Model_Gettext::gettextCheck($configSQL);
-	        				$options = array();
-	        				if (isset($chart->options)){
-		        				foreach ($chart->options->toArray() as $key => $value){
-		        					if(!is_array($value)){
-		        						$options[$key] = Klear_Model_Gettext::gettextCheck($value);
-		        					} else {
-		        						foreach ($value as $key2 => $value2){
-		        							$options[$key][$key2] = Klear_Model_Gettext::gettextCheck($value2);
-		        						}
-		        					}
-
-		        				}
-	        				}
-	        				$gChart = array();
-	        				$gChart["title"] = $chartTitle;
-	        				if(isset($chart->comment)){
-	        					$gChart["comment"] = Klear_Model_Gettext::gettextCheck($chart->comment);
-	        				}
-	        				if(isset($chart->legend)){
-	        					$gChart["legend"] = Klear_Model_Gettext::gettextCheck($chart->legend);
-	        				}
-	        				if (isset($chart->type)){
-	        					$gChart["type"] = $chart->type;
-	        				} else {
-	        					$gChart["type"] = "ColumnChart";
-	        				}
-
-	        				$gChart["hAxis"] = array();
-	        				$gChart["hAxis"]["values"] = array ();
-	        				$gChart["vAxis"] = array();
-	        				$gChart["vAxis"]["values"] = array ();
-	        				$dbAdapter = Zend_Db_Table::getDefaultAdapter();
-	        				$stmt = $dbAdapter->query($sql);
-	        				$results = $stmt->fetchAll();
-	        				$gChart["table"] = array();
-	        				$table = array();
-	        				if(count($results)>0){
-	        					$tableHeaders = array_keys($results[0]);
-	        					$tableHeadersTranslated = array();
-								if(isset($chart->cols)){
-									foreach ($tableHeaders as $tableHeader){
-										if(isset($chart->cols->$tableHeader)){
-											$tableHeadersTranslated[] = Klear_Model_Gettext::gettextCheck($chart->cols->$tableHeader);
-										}else {
-											$tableHeadersTranslated[] = $tableHeader;
-										}
-									}
-								} else {
-									$tableHeadersTranslated = $tableHeaders;
-								}
-								$table[] = $tableHeadersTranslated;
-	        					foreach ($results as $row){
-	        						$rowN = array();
-	        						foreach($row as $field){
-	        							if (is_numeric($field)){
-	        								$field = floatval($field);
-	        							} else {
-	        								$field = $this->_helper->translate($field);
-	        							}
-	        							$rowN[] = $field;
-	        						}
-	        						$table[] = $rowN;
-	        					}
-	        				}
-	        				$gChart["table"] = $table;
-	        				$gChart["options"] = $options;
-	        				$group["charts"][] = $gChart;
-
-	        			}
-	        		}
-        		}
-        		$data["chartGroups"][] = $group;
-        	}
-        }
-
-        $jsonResponse = KlearMatrix_Model_DispatchResponseFactory::build();
-        $jsonResponse->setPlugin("googlecharts");
-        $jsonResponse->addTemplate("/template/googlecharts", "klearmatrixGooglecharts");
-        $jsonResponse->addJsFile("/js/plugins/jquery.klearmatrix.googlecharts.js");
-        $jsonResponse->addCssFile("/css/googlecharts.css");
-        $jsonResponse->setData($data);
-        $jsonResponse->attachView($this->view);*/
     }
 }

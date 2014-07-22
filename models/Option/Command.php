c9e5d337e784a462234a1a275c35fd5d53ffff3d
Luis Felipe García <lfgarcia@irontec.com>
2014-07-22 12:08:05 +0000

    #41364 arreglado el problema del command option en un custom dialog
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@906 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Option/Command.php
+++ models/Option/Command.php
@@ -24,8 +24,10 @@ class KlearMatrix_Model_Option_Command extends KlearMatrix_Model_Option_Abstract
         $ret['type'] = 'command';
         $ret['external'] = true;
 
-        if ($this->getConfig()->getProperty('disabledTime')) {
-            $ret['disabledTime'] = $this->getConfig()->getProperty('disabledTime');
+        if ($this->getConfig()) {
+            if ($this->getConfig()->getProperty('disabledTime')) {
+                $ret['disabledTime'] = $this->getConfig()->getProperty('disabledTime');
+            }
         }
 
         if ($this->isDefault()) {

96069ea5c6c516652ecbee490d81060168e51ed2
Mikel Madariaga <mikel@irontec.com>
2015-02-04 18:44:45 +0000

    #45862 Aceptando el atributo data-autofilter-select-by-data en array cuando debe ser aplicado sobre n elementos
    git-svn-id: http://dev.irontec.com/svn/klearMatrix/trunk/modules/klearMatrix@956 6214b6a1-08aa-4791-a4b5-1ce3821c3f96

--- models/Field/Select.php
+++ models/Field/Select.php
@@ -36,10 +36,41 @@ class KlearMatrix_Model_Field_Select extends KlearMatrix_Model_Field_Abstract
 
         return $this->_column->getDbFieldName();
     }
-    
+
     public function isMassUpdateable()
     {
         return true;
+    }
+
+    protected function _getAttributes($fieldConfig)
+    {
+        $fieldConfig = parent::_getAttributes($fieldConfig);
+        return $this->_prepareFieldCustomAttributes($fieldConfig);
+    }
+
+    protected function _prepareFieldCustomAttributes($fieldConfig)
+    {
+        return $this->_prepareAutofilterAttribute($fieldConfig);        
+    }
+
+    protected function _prepareAutofilterAttribute($fieldConfig)
+    {
+        $autoFilterKey = 'data-autofilter-select-by-data';
+        if (
+            !is_array($fieldConfig) || 
+            !array_key_exists($autoFilterKey, $fieldConfig) ||
+            !is_array($fieldConfig[$autoFilterKey])
+        ) {
+            return $fieldConfig;
+        }
+
+        $autofilter = array();
+        foreach ($fieldConfig[$autoFilterKey] as $autofilterElement) {
+            $autofilter[] = key($autofilterElement) . ':' . current($autofilterElement);
+        }
+
+        $fieldConfig[$autoFilterKey] = implode("|", $autofilter);
+        return $fieldConfig;
     }
 }
 

5222fb0e5cd1e15865a431ecdaad33631e1fb7f1
Luis García <lfgarcia@irontec.com>
2015-09-21 13:22:59 +0200

    Creado campo Ghost Image para mostrar imágenes de Gravatar #52417
    Se ha creado el campo Ghost Image que crea un tag img cuyo src se forma
    a partir de un campo de la base de datos y configuraciónes del yaml.
    
    De esta manera se pueden mostrar, entre otras, imágenes de Gravatar.
    
    La configuración en el modelo es la siguiente:
    
    production:
      class: \Proyecto\Model\Entity
      fields:
        gravatar:
          title: _('Gravatar')
          type: ghost
          source:
            predefined: image
            url: "http://www.gravatar.com/avatar/"
            idField: email
            md5IdField: false   #Se puede omitir. Por defecto true
            params:             #Se puede omitir. Se pasan los parámetros
              s: 200            #tal cual a la URL de forma que quedaría,
                                #en este caso la URL así:
                                #http://www.gravatar.com/avatar/mi@email.com?s=200

---
+++ models/Field/Ghost/Image.php
@@ -0,0 +1,77 @@
+<?php
+class KlearMatrix_Model_Field_Ghost_Image extends KlearMatrix_Model_Field_Ghost_Abstract
+{
+    protected $_config;
+    protected $_parentField;
+
+    protected $_templateFields = array();
+
+    protected $_searchedValues;
+
+    protected $_idField;
+    protected $_md5Field;
+    protected $_imageURL;
+    protected $_imageId;
+    protected $_urlParams = null;
+
+    public function setConfig(Zend_Config $config)
+    {
+        $kconfig = new Klear_Model_ConfigParser;
+        $kconfig->setConfig($config);
+
+        $this->_config = $kconfig;
+        return $this;
+    }
+
+    public function configureHostFieldConfig(KlearMatrix_Model_Field_Abstract $field)
+    {
+        $this->_parentField = $field;
+        $this->_parentField->setSearchMethod('getSearch');
+        $this->_parentField->setOrderMethod('getOrder');
+        $this->_parentField->setGetterMethod('getValue');
+        $this->_parentField->getColumn()->markAsDirty();
+
+        return $this;
+    }
+
+    public function init()
+    {
+        if (!$this->_parentField) {
+            throw new Klear_Exception_MissingConfiguration('Missing parent host for Ghost_Image');
+        }
+
+        $mainModel = $this->_parentField->getColumn()->getModel();
+
+        $this->_imageURL = $this->_config->getRaw()->source->url;
+        $this->_idField = $this->_config->getRaw()->source->idField;
+        $this->_md5IdField = true;
+        if (isset($this->_config->getRaw()->source->md5IdField)) {
+            $this->_md5IdField = $this->_config->getRaw()->source->md5IdField;
+        }
+        if (isset($this->_config->getRaw()->source->params)) {
+            $this->_urlParams = $this->_config->getRaw()->source->params;
+        }
+
+    }
+
+    public function getValue($model)
+    {
+        $idFieldGetter = 'get' . $model->columnNameToVar($this->_idField);
+        $imageId = $model->$idFieldGetter();
+
+        $paramsString = "";
+        if (!is_null($this->_urlParams)) {
+            $params = array();
+            foreach ($this->_urlParams as $key => $value) {
+                $params[] = $key."=".$value;
+            }
+            $paramsString = "?".implode("&", $params);
+        }
+
+        if ($this->_md5IdField) {
+            $imageId = md5($imageId);
+        }
+           return '<img src="'.$this->_imageURL.$imageId.$paramsString.'" />';
+    }
+
+}

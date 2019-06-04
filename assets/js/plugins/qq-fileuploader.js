8ffb68716cfb5c65d510a1875b4d294b096e3c0d
mmadariaga <mikel@irontec.com>
2019-06-04 14:00:21 +0200

    hide error message on upload dialog

--- assets/js/plugins/qq-fileuploader.js
+++ assets/js/plugins/qq-fileuploader.js
@@ -629,7 +629,7 @@ qq.FileUploader = function(o){
             '<span class="qq-upload-file"></span>' +
             '<span class="qq-upload-size"></span>' +
             '<a class="qq-upload-cancel" href="#">{cancelButtonText}</a>' +
-            '<span class="qq-upload-failed-text">{failUploadtext}</span>' +
+            '<span class="qq-upload-failed-text" style="display:none;">{failUploadtext}</span>' +
             '</li>',
 
         classes: {
